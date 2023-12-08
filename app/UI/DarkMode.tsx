38e3eff0c58d37c28e351355c137258ae63e24d9
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-08 05:47:18 +0000

    Update 4 files

---
+++ app/UI/DarkMode.tsx
@@ -0,0 +1,43 @@
+"use client";
+import Image from "next/image";
+
+import React, { useState, useEffect } from "react";
+import sun from "../assets/sun.svg";
+import moon from "../assets/moon.svg";
+
+const ToggleDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const handleDarkModeToggle = () => {
+    const newDarkMode = !isDarkMode;
+    setIsDarkMode(newDarkMode);
+    localStorage.setItem("theme", newDarkMode ? "dark" : "light");
+    document.documentElement.classList.toggle("dark", newDarkMode);
+  };
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setIsDarkMode(savedTheme === "dark");
+      document.documentElement.classList.toggle("dark", savedTheme === "dark");
+    }
+  }, []);
+
+  return (
+    <div className="flex relative p-1 rounded-[10px] border border-zinc-300 dark:border-zinc-700 bg-zinc-300 dark:bg-zinc-900 items-center justify-center">
+      <input
+        className="opacity-0"
+        id="toggle-darkmode"
+        type="checkbox"
+        checked={isDarkMode}
+        onChange={handleDarkModeToggle}
+      />
+      <div className="dark-icon flex  items-center justify-center  absolute">
+        <Image className="sun" src={sun} alt="sun" />
+        <Image className="moon" src={moon} alt="moon" />
+      </div>
+    </div>
+  );
+};
+
+export default ToggleDarkMode;
