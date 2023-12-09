22393f6ad57f4858d1f23e98bac1847ddfca6162
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-09 03:31:23 +0000

    Update Hero.tsx and data.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,28 +1,48 @@
+"use client";
 import AboutmeButton from "../UI/aboutme";
 import ContactmeButton from "../UI/contact";
 import { personaldata } from "../constant/data";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
 export default function Hero() {
   return (
     <section className="w-full px-5 py-[9rem]">
       <div className="max-w-screen-lg mx-auto flex items-center justify-between gap-11 max-[600px]:flex-col max-[600px]:items-start">
-        <Image
-          className="grayscale hover:grayscale-0 transition-all ease-linear duration-500"
-          src={"/people.webp"}
-          width={500}
-          height={500}
-          alt="people"
-        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1 }}
+        >
+          <Image
+            className="grayscale hover:grayscale-0 transition-all ease-linear duration-500"
+            src={"/people.webp"}
+            width={500}
+            height={500}
+            alt="people"
+          />
+        </motion.div>
         {personaldata.map((personal) => (
           <div key={personal.key}>
-            <h1 className="text-[4rem] leading-none font-semibold pb-6">
+            <motion.h1
+              className="text-[4rem] leading-none font-semibold pb-6"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 1, duration: 3 }}
+            >
               I'm {personal.fullName}.
-            </h1>
+            </motion.h1>
             <ul className="list-disc pl-3 ml-3 grid gap-3">
               {personal.experties.map((experties, index) => (
-                <li className="font-medium text-zinc-300" key={index}>
+                <motion.li
+                  className="font-medium text-zinc-300"
+                  key={index}
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: 1, duration: 3 }}
+                >
                   {experties}
-                </li>
+                </motion.li>
               ))}
             </ul>
             <div className="flex items-center gap-3 pt-[2rem]">
