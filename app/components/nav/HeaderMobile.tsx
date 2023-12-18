74fae8a71321ee3c2bf82e9c549ef89d21f9317a
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-18 14:08:24 +0000

    Update HeaderMobile.tsx and MobileNavigation.tsx

--- app/components/nav/HeaderMobile.tsx
+++ app/components/nav/HeaderMobile.tsx
@@ -1,34 +1,34 @@
 "use client";
 import { useState, useEffect } from "react";
-import MobileNavigation from "../../components/nav/MobileNavigation"
+import MobileNavigation from "../../components/nav/MobileNavigation";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
 export function Hamburger({ onToggle }) {
-    return (
-      <div className="lg:hidden md:flex">
-        <button
-          className="cursor-pointer flex items-center gap-2"
-          onClick={onToggle}
-        >
-          <span>Menu</span>
-          <Image src={"/menu.svg"} width={30} height={30} alt="menu" />
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="lg:hidden md:flex">
+      <button
+        className="cursor-pointer flex items-center gap-2"
+        onClick={onToggle}
+      >
+        <span>Menu</span>
+        <Image src={"/menu.svg"} width={30} height={30} alt="menu" />
+      </button>
+    </div>
+  );
+}
 
-export default function HeaderMobile(){
-    const [isMobile, setIsMobile] = useState(false);
-    const [isOpen, setIsOpen] = useState(false);
+export default function HeaderMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 600);
     };
 
-    handleResize(); 
+    handleResize();
     window.addEventListener("resize", handleResize);
 
     return () => {
@@ -43,31 +43,28 @@ export default function HeaderMobile(){
   const closeMenu = () => {
     setIsOpen(false);
   };
-    return(
-        <header className="lg:hidden max-[600px]:flex items-center gap-3 fixed p-5 justify-between w-full bg-zinc-900 z-[50]">
-            <motion.div
-      className="flex items-center "
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.5, duration: 1 }}
-        >
-          <Image
-            className="grayscale hover:grayscale-0 transition-all ease-linear duration-500 rounded-lg max-[600px]:w-[50px] max-[600px]:h-[50px]"
-            src={"/people.webp"}
-            width={200}
-            height={200}
-            alt="people"
-          />
-          <div className="flex items-center flex-col">
-            <h1 className="font-extrabold text-3xl pt-3">Bob.</h1>
-            <h3 className="text-zinc-400">Web Designer</h3>
-          </div>
-        </motion.div>
-        {isMobile && isOpen ? (
-          <MobileNavigation closeMenu={closeMenu} />
-        ) : null}
-          <Hamburger onToggle={handleToggle} />
-
-        </header>
-    )
+  return (
+    <header className="lg:hidden max-[600px]:flex items-center gap-3 fixed px-5 py-3 justify-between w-full bg-zinc-900 z-[50]">
+      <motion.div
+        className="flex items-center "
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5, duration: 1 }}
+      >
+        <Image
+          className="grayscale hover:grayscale-0 transition-all ease-linear duration-500 rounded-lg max-[600px]:w-[50px] max-[600px]:h-[50px]"
+          src={"/people.webp"}
+          width={200}
+          height={200}
+          alt="people"
+        />
+        <div className="flex items-start flex-col pl-6">
+          <h1 className="font-extrabold text-3xl pt-3 max-[600px]:text-2xl">Bob.</h1>
+          <h3 className="text-zinc-400">Web Designer</h3>
+        </div>
+      </motion.div>
+      {isMobile && isOpen ? <MobileNavigation closeMenu={closeMenu} /> : null}
+      <Hamburger onToggle={handleToggle} />
+    </header>
+  );
 }
