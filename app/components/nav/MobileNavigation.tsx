faf47895e5d24ad5a6aa03ae0f681851407fce4a
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-18 12:06:27 +0000

    Update MobileNavigation.tsx

--- app/components/nav/MobileNavigation.tsx
+++ app/components/nav/MobileNavigation.tsx
@@ -1,15 +1,18 @@
-
-
 import Link from "next/link";
 import { NAVIGATION } from "../../constant/data";
 
 const MobileNavigation = ({ closeMenu }) => (
-  <div className="absolute top-25 left-0 grid gap-8 p-5 w-full bg-zinc-900 -z-10 rounded-b-lg">
+  <div className="absolute top-22 left-0 grid gap-8 p-5 w-full bg-zinc-900 -z-10 rounded-b-lg">
     <div className="grid gap-1 w-full">
       {NAVIGATION.map((link) => (
-        <Link key={link.key} href={link.href} className="group-hover:text-white NavHover flex items-center gap-3" onClick={closeMenu}>
+        <Link
+          key={link.key}
+          href={link.href}
+          className="group-hover:text-white NavHover flex items-center gap-3"
+          onClick={closeMenu}
+        >
           <div className="fill-white">{link.icon}</div>
-            <span>{link.title}</span>
+          <span>{link.title}</span>
         </Link>
       ))}
     </div>
